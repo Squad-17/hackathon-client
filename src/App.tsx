a11c730991d7d35c2096b05d7dd89adbbf639053
Anderson <andersonfonseca1999@hotmail.com>
2021-09-06 13:48:07 -0300

    Adiciona authProvider e rotas no componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import GlobalStyles from './styles/global';
 
+import { AuthProvider } from './hooks/auth';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
+
 function App() {
   return (
-    <>
-      <div></div>
-      <GlobalStyles />
-    </>
+    <Router>
+      <AuthProvider>
+        <Routes />
+        <GlobalStyles />
+      </AuthProvider>
+    </Router>
   );
 }
 
