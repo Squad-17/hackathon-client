6cacb8301095d5c427b1377a9fc5fd8be16803b4
Anderson <andersonfonseca1999@hotmail.com>
2021-09-07 20:41:14 -0300

    Refatora página de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,94 +1,30 @@
-import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router';
 
-import { useAuth } from '../../hooks/auth';
-import { FieldErrors, signInValidate } from '../../utils/validations';
+import FormSignIn from '../../components/FormSignIn';
 
 import * as S from './styles';
 
 export default function SignIn() {
-  const [formError, setFormError] = useState('');
-  const [fieldErrors, setFieldErrors] = useState<FieldErrors>({});
-  const [values, setValues] = useState({
-    email: '',
-    password: '',
-  });
-  const { signIn } = useAuth();
   const history = useHistory();
-
-  function handleSubmit(e: FormEvent) {
-    e.preventDefault();
-
-    setFormError('');
-
-    const errors = signInValidate(values);
-
-    if (Object.keys(errors).length) {
-      setFieldErrors(errors);
-
-      return;
-    }
-
-    setFieldErrors({});
-
-    const { email, password: senha } = values;
-
-    signIn({ email, senha })
-      .then(() => {
-        history.push('/agendar');
-      })
-      .catch((error) => {
-        setFormError(error.response.data.erro);
-      });
-  }
-
-  function handleInput(field: string, value: string) {
-    setValues((s) => ({ ...s, [field]: value }));
-  }
 
   function goToSignUp() {
     history.push('/cadastrar');
   }
 
   return (
-    <div>
-      <h1>Sign In</h1>
+    <S.Wrapper>
+      <S.Heading>Área de login</S.Heading>
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='email'>Email:</label>
-          <input
-            type='email'
-            placeholder='email'
-            id='email'
-            onChange={(e) => handleInput('email', e.target.value)}
-          />
+      <S.FormWrapper>
+        <FormSignIn />
 
-          {fieldErrors.email && (
-            <S.FieldError>{fieldErrors.email}</S.FieldError>
-          )}
-        </div>
-
-        <div>
-          <label htmlFor='password'>Senha:</label>
-          <input
-            type='password'
-            placeholder='password'
-            id='password'
-            onChange={(e) => handleInput('password', e.target.value)}
-          />
-
-          {fieldErrors.password && (
-            <S.FieldError>{fieldErrors.password}</S.FieldError>
-          )}
-        </div>
-
-        <button type='submit'>Enviar</button>
-      </form>
-
-      {formError && <S.FormError>{formError}</S.FormError>}
-
-      <button onClick={goToSignUp}>Não está cadastrado? Cadastre-se</button>
-    </div>
+        <S.CallSignUp>
+          <p>
+            Não possui uma conta?{' '}
+            <span onClick={goToSignUp}>Cadastre-se aqui</span>
+          </p>
+        </S.CallSignUp>
+      </S.FormWrapper>
+    </S.Wrapper>
   );
 }
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,9 +1,30 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const FieldError = styled.p`
-  color: red;
+export const Wrapper = styled.div``;
+
+export const Heading = styled.h2`
+  ${({ theme }) => css`
+    color: ${theme.colors.primary};
+    font-size: ${theme.font.sizes.xlarge};
+    margin-bottom: ${theme.spacings.large};
+  `}
 `;
 
-export const FormError = styled.p`
-  color: red;
+export const FormWrapper = styled.div`
+  width: 100%;
+  max-width: 40rem;
+`;
+
+export const CallSignUp = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    font-size: ${theme.font.sizes.medium};
+
+    span {
+      color: ${theme.colors.secondary};
+      cursor: pointer;
+      font-weight: 900;
+    }
+  `}
 `;
