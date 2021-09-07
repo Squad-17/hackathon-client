0a89365a0f1b752c44ef24676185240e510dffa0
Anderson <andersonfonseca1999@hotmail.com>
2021-09-07 20:40:42 -0300

    Refatora página de cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,122 +1,29 @@
-import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router';
 
-import { useAuth } from '../../hooks/auth';
-import { FieldErrors, signUpValidate } from '../../utils/validations';
+import FormSignUp from '../../components/FormSignUp';
 
 import * as S from './styles';
 
 export default function SignUp() {
-  const [formError, setFormError] = useState('');
-  const [fieldErrors, setFieldErrors] = useState<FieldErrors>({});
-  const [values, setValues] = useState({
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-  });
-  const { signUp } = useAuth();
   const history = useHistory();
-
-  function handleSubmit(e: FormEvent) {
-    e.preventDefault();
-
-    setFormError('');
-
-    const errors = signUpValidate(values);
-
-    if (Object.keys(errors).length) {
-      setFieldErrors(errors);
-
-      return;
-    }
-
-    setFieldErrors({});
-
-    const { name: nome, email, password: senha } = values;
-
-    signUp({ nome, email, senha })
-      .then(() => {
-        history.push('/agendar');
-      })
-      .catch((error) => {
-        setFormError(error.response.data.erro);
-      });
-  }
-
-  function handleInput(field: string, value: string) {
-    setValues((s) => ({ ...s, [field]: value }));
-  }
 
   function goToSignIn() {
     history.push('/login');
   }
 
   return (
-    <div>
-      <h1>Sign Up</h1>
+    <S.Wrapper>
+      <S.Heading>Cadastre-se</S.Heading>
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='name'>Nome:</label>
-          <input
-            type='name'
-            placeholder='name'
-            id='name'
-            onChange={(e) => handleInput('name', e.target.value)}
-          />
+      <S.FormWrapper>
+        <FormSignUp />
 
-          {fieldErrors.name && <S.FieldError>{fieldErrors.name}</S.FieldError>}
-        </div>
-
-        <div>
-          <label htmlFor='email'>Email:</label>
-          <input
-            type='email'
-            placeholder='email'
-            id='email'
-            onChange={(e) => handleInput('email', e.target.value)}
-          />
-
-          {fieldErrors.email && (
-            <S.FieldError>{fieldErrors.email}</S.FieldError>
-          )}
-        </div>
-
-        <div>
-          <label htmlFor='password'>Senha:</label>
-          <input
-            type='password'
-            placeholder='password'
-            id='password'
-            onChange={(e) => handleInput('password', e.target.value)}
-          />
-
-          {fieldErrors.password && (
-            <S.FieldError>{fieldErrors.password}</S.FieldError>
-          )}
-        </div>
-
-        <div>
-          <label htmlFor='confirmPassword'>Confirmar senha:</label>
-          <input
-            type='password'
-            placeholder='confirmPassword'
-            id='confirmPassword'
-            onChange={(e) => handleInput('confirmPassword', e.target.value)}
-          />
-
-          {fieldErrors.confirmPassword && (
-            <S.FieldError>{fieldErrors.confirmPassword}</S.FieldError>
-          )}
-        </div>
-
-        <button type='submit'>Enviar</button>
-      </form>
-
-      {formError && <S.FormError>{formError}</S.FormError>}
-
-      <button onClick={goToSignIn}>já está cadastrado, faça o login.</button>
-    </div>
+        <S.CallSignIn>
+          <p>
+            Já possui uma conta? <span onClick={goToSignIn}>Faça o login</span>
+          </p>
+        </S.CallSignIn>
+      </S.FormWrapper>
+    </S.Wrapper>
   );
 }
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,9 +1,30 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const FieldError = styled.p`
-  color: red;
+export const Wrapper = styled.div``;
+
+export const Heading = styled.h2`
+  ${({ theme }) => css`
+    margin-bottom: ${theme.spacings.large};
+    color: ${theme.colors.primary};
+    font-size: ${theme.font.sizes.xlarge};
+  `}
 `;
 
-export const FormError = styled.p`
-  color: red;
+export const FormWrapper = styled.div`
+  width: 100%;
+  max-width: 40rem;
+`;
+
+export const CallSignIn = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    font-size: ${theme.font.sizes.medium};
+
+    span {
+      color: ${theme.colors.secondary};
+      cursor: pointer;
+      font-weight: 900;
+    }
+  `}
 `;
