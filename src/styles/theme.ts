416316ec76bad06778a3f6ccce4ab372c849e8d9
Anderson <andersonfonseca1999@hotmail.com>
2021-09-07 18:16:49 -0300

    Adiciona tema para o css

---
+++ src/styles/theme.ts
@@ -0,0 +1,33 @@
+export default {
+  font: {
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+    },
+  },
+  colors: {
+    primary: '#FE4400',
+    secondary: '#404099',
+    white: '#FFFFFF',
+    black: '#000000',
+    red: '#FF6347',
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50,
+  },
+} as const;
