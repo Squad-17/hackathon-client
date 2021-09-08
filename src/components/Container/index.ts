11bd4d2168f5e2f5499f81a67ed7e34cd6cbc037
Matheus Carvalho <matheus10277@gmail.com>
2021-09-08 14:34:08 -0300

    Criando estilo padrão para container

---
+++ src/components/Container/index.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 120rem;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+`;
