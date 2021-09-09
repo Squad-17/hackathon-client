bb75ef374b350d6988e7ae3a666e6fdfe2c3f7d3
Anderson <andersonfonseca1999@hotmail.com>
2021-09-09 16:06:44 -0300

    Corrige import do componente Container

--- src/components/Protocols/index.tsx
+++ src/components/Protocols/index.tsx
@@ -1,13 +1,13 @@
-import { ReactComponent as Mask } from "../../assets/protocols/mask.svg";
-import { ReactComponent as Eyes } from "../../assets/protocols/eyes.svg";
-import { ReactComponent as Clean } from "../../assets/protocols/clean.svg";
-import { ReactComponent as Hands } from "../../assets/protocols/hands.svg";
-import { ReactComponent as Distance } from "../../assets/protocols/distance.svg";
-import { ReactComponent as Temperature } from "../../assets/protocols/temperature.svg";
+import { ReactComponent as Mask } from '../../assets/protocols/mask.svg';
+import { ReactComponent as Eyes } from '../../assets/protocols/eyes.svg';
+import { ReactComponent as Clean } from '../../assets/protocols/clean.svg';
+import { ReactComponent as Hands } from '../../assets/protocols/hands.svg';
+import { ReactComponent as Distance } from '../../assets/protocols/distance.svg';
+import { ReactComponent as Temperature } from '../../assets/protocols/temperature.svg';
 
-import { Container } from "../Container";
+import Container from '../Container';
 
-import * as S from "./styles";
+import * as S from './styles';
 
 export default function Protocols() {
   return (
@@ -15,30 +15,30 @@ export default function Protocols() {
       <S.Wrapper>
         <h1>Protocolos e Procedimentos</h1>
 
-        <ul className="protocols">
-          <li className="protocol">
+        <ul className='protocols'>
+          <li className='protocol'>
             <Mask />
-            <p className="blue">Sempre use máscara</p>
+            <p className='blue'>Sempre use máscara</p>
           </li>
-          <li className="protocol">
+          <li className='protocol'>
             <Temperature />
-            <p className="orange">Verifique sua temperatura</p>
+            <p className='orange'>Verifique sua temperatura</p>
           </li>
-          <li className="protocol">
+          <li className='protocol'>
             <Eyes />
-            <p className="blue">Não toque olhos, nariz e boca</p>
+            <p className='blue'>Não toque olhos, nariz e boca</p>
           </li>
-          <li className="protocol">
+          <li className='protocol'>
             <Hands />
-            <p className="orange">Higienize as mãos frequentemente</p>
+            <p className='orange'>Higienize as mãos frequentemente</p>
           </li>
-          <li className="protocol">
+          <li className='protocol'>
             <Clean />
-            <p className="blue">Higienize superfícies e espaços</p>
+            <p className='blue'>Higienize superfícies e espaços</p>
           </li>
-          <li className="protocol">
+          <li className='protocol'>
             <Distance />
-            <p className="orange">Mantenha uma distância segura</p>
+            <p className='orange'>Mantenha uma distância segura</p>
           </li>
         </ul>
       </S.Wrapper>
