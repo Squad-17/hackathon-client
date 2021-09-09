ef7c576462fa15ff3af41bfb8a35d1b77d6a4447
Matheus Carvalho <matheus10277@gmail.com>
2021-09-09 17:58:09 -0300

    Criando componente de passos do form de agendamento

--- src/components/FormStepBar/index.tsx
+++ src/components/FormStepBar/index.tsx
@@ -1,7 +1,17 @@
-export default function FormStepBar() {
-    return (
-        <div>
-            Form step
-        </div>
+import * as S from './styles';
+
+type FormStepBarProps = {
+    currentStep: number;
+}
+
+export default function FormStepBar({currentStep}: FormStepBarProps) {    
+    return (        
+        <S.Wrapper>
+            <S.Step className={currentStep === 1 ? "active" : ""} />
+            <S.Connector />
+            <S.Step className={currentStep === 2 ? "active" : ""} />
+            <S.Connector />
+            <S.Step className={currentStep === 3 ? "active" : ""} />            
+        </S.Wrapper>
     )
 }
--- src/pages/Scheduling/index.tsx
+++ src/pages/Scheduling/index.tsx
@@ -1,14 +1,17 @@
+import Container from '../../components/Container';
+import FormStepBar from '../../components/FormStepBar';
 import { useAuth } from '../../hooks/auth';
 
 export default function Scheduling() {
   const { name, signOut } = useAuth();
 
   return (
-    <div>
+    <Container>
+      <FormStepBar currentStep={3}/>
       <h1>Agendamento</h1>
       <h2>Olá, {name}</h2>
 
       <button onClick={signOut}>signOut</button>
-    </div>
+    </Container>
   );
 }
