f75514f8ae7c8374a8c83d54f3bdf2671b6e643d
Anderson <andersonfonseca1999@hotmail.com>
2021-09-10 15:46:49 -0300

    Adiciona componente de confirmação de agendamento

---
+++ src/components/SchedulingConfirmation/styles.ts
@@ -0,0 +1,55 @@
+import styled, { css } from 'styled-components';
+
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    height: 52.5rem;
+    background: #fbfbfb;
+    border: solid 0.1rem ${theme.colors.secondary};
+    border-radius: 1rem;
+    padding: ${theme.spacings.xsmall};
+    font-size: ${theme.font.sizes.medium};
+  `}
+`;
+
+export const Title = styled.h2`
+  ${({ theme }) => css`
+    color: ${theme.colors.secondary};
+    font-size: ${theme.font.sizes.xlarge};
+    text-align: center;
+  `}
+`;
+
+export const Local = styled.div`
+  ${({ theme }) => css`
+    margin-top: ${theme.spacings.medium};
+
+    p {
+      display: block;
+      margin-top: ${theme.spacings.xxsmall};
+    }
+  `}
+`;
+
+export const Date = styled.div`
+  ${({ theme }) => css`
+    margin-top: ${theme.spacings.medium};
+
+    p {
+      display: block;
+      margin-top: ${theme.spacings.xxsmall};
+    }
+  `}
+`;
+
+export const ButtonWrapper = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    margin-top: ${theme.spacings.large};
+
+    button:not(:first-child) {
+      margin-left: ${theme.spacings.xsmall};
+    }
+  `}
+`;
