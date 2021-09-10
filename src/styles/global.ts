99b9ca074ed90748555bdfd1ead30c6d3e3437ca
Re04nan <renan04_marques@hotmail.com>
2021-09-10 16:38:30 -0300

    Configurando webpack.config.ts e adicionando a fonte metropolis no projeto

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,30 @@
 import { createGlobalStyle } from 'styled-components';
+import MetropolisBlack from '../assets/fonts/Metropolis-Black.otf';
+import MetropolisBold from '../assets/fonts/Metropolis-Bold.otf';
+import MetropolisRegular from '../assets/fonts/Metropolis-Regular.otf';
+import MetropolisLight from '../assets/fonts/Metropolis-Light.otf';
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Metropolis Black';
+    src: url(${MetropolisBlack});
+  }
+  @font-face {
+    font-family: 'Metropolis Bold';
+    src: url(${MetropolisBold});
+  }
+  @font-face {
+    font-family: 'Metropolis Regular';
+    src: url(${MetropolisRegular});
+  }
+  @font-face {
+    font-family: 'Metropolis Light';
+    src: url(${MetropolisLight});
+  }
   * {
     margin: 0;
     padding: 0;
+    font-family: 'Metropolis Bold', sans-serif;
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
@@ -18,6 +39,6 @@ const GlobalStyles = createGlobalStyle`
     font-size: 62.5%;
     background: #FEFEFE;
   }
-`;
+`
 
 export default GlobalStyles;
