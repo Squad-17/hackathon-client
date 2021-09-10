de4ecd9d249a8a1135af7012159cf051f8f11b4d
Matheus Carvalho <matheus10277@gmail.com>
2021-09-10 14:59:59 -0300

    Implementando primeira parte da dashboard

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,33 @@
+import Nav from "../../components/Nav";
+import Button from "../../components/Button";
+import Container from "../../components/Container";
+import NextSchedules from "../../components/NextSchedulesList";
+
+import * as S from "./styles";
+import { ReactComponent as Avatar } from "../../assets/guy-1.svg";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <Container>
+      <S.Wrapper>
+        <Nav />
+
+        <S.Title>Dashboard</S.Title>
+
+        <S.GridWrapper>
+          <S.ProfileWrapper>
+            <Avatar className="profile-avatar" />
+
+            <S.ProfileTag className="blue"> #ProudToBeOrange</S.ProfileTag>
+            <S.ProfileTag className="orange">Henrique Lopes</S.ProfileTag>
+            <S.ProfileTag className="orange">Dev. Front-End</S.ProfileTag>
+          </S.ProfileWrapper>
+
+          <NextSchedules />
+        </S.GridWrapper>
+
+        <Button>Agendar</Button>
+      </S.Wrapper>
+    </Container>
+  );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,62 @@
-import styled from 'styled-components';
+import styled, { css } from "styled-components";
+
+export const Title = styled.h1`
+  ${({ theme }) => css`
+    font-weight: 900;
+    font-size: 3.6rem;
+    line-height: 3.6rem;
+    margin-bottom: 5rem;
+    color: ${theme.colors.primary};
+  `}
+`;
+
+export const Wrapper = styled.div`
+  display: grid;
+  height: 100vh;
+  max-height: 100vh;
+
+  button {
+    margin-top: 5rem;
+    margin-left: auto;
+    display: inline-block;
+  }
+`;
+
+export const GridWrapper = styled.div`
+  display: grid;
+  column-gap: 10rem;
+  grid-template-columns: auto 1fr;
+`;
+
+export const ProfileWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  padding: 8rem 12rem;
+  background: #f3f4f9;
+  border-radius: 21px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+
+  .profile-avatar {
+    margin-bottom: 5rem;
+  }
+`;
+
+export const ProfileTag = styled.p`
+  ${({ theme }) => css`
+    color: #fff;
+    padding: 1rem 1.5rem;
+    border-radius: 11px;
+    font-size: ${theme.font.sizes.xlarge};
+    margin-bottom: ${theme.spacings.medium};
+
+    &.orange {
+      background: ${theme.colors.primary};
+    }
+
+    &.blue {
+      background: ${theme.colors.secondary};
+    }
+  `}
+`;
