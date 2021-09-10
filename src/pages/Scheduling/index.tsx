703da60b4c3aa39d6a38f766ff025833473a1271
Anderson <andersonfonseca1999@hotmail.com>
2021-09-10 15:48:34 -0300

    Implementa lógica na página de agendamento

--- src/pages/Scheduling/index.tsx
+++ src/pages/Scheduling/index.tsx
@@ -1,17 +1,47 @@
+import { useState } from 'react';
+
 import Container from '../../components/Container';
 import FirstStepScheduling from '../../components/FirstStepScheduling';
 import FormStepBar from '../../components/FormStepBar';
+import SchedulingConfirmation from '../../components/SchedulingConfirmation';
 
 import * as S from './styles';
 
+export type Informations = {
+  data: string;
+  localId: number;
+  cidade: string;
+  endereco: string;
+  diaDaSemana: string;
+  diaDoMes: number;
+  disponivel: boolean;
+};
+
 export default function Scheduling() {
+  const [currentStep, setCurrentStep] = useState(1);
+  const [informations, setInformations] = useState<Informations>(
+    {} as Informations
+  );
+
   return (
     <Container>
       <S.Wrapper>
-        <FormStepBar currentStep={1} />
+        <FormStepBar currentStep={currentStep} />
 
         <S.SchedulingWrapper>
-          <FirstStepScheduling />
+          {currentStep === 1 && (
+            <FirstStepScheduling
+              setCurrentStep={setCurrentStep}
+              setInformations={setInformations}
+            />
+          )}
+
+          {currentStep === 2 && (
+            <SchedulingConfirmation
+              setCurrentStep={setCurrentStep}
+              informations={informations}
+            />
+          )}
         </S.SchedulingWrapper>
       </S.Wrapper>
     </Container>
