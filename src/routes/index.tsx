591cdc027b4e14ba3d47cbb35cf142890e9ae181
Anderson <andersonfonseca1999@hotmail.com>
2021-09-11 22:03:37 -0300

    Adiciona rota de cancelar agendamento

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import Home from '../pages/Home';
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
 import Scheduling from '../pages/Scheduling';
+import CancelScheduling from '../pages/CancelScheduling';
 import PageNotFound from '../pages/404';
 
 export default function Routes() {
@@ -14,6 +15,11 @@ export default function Routes() {
       <Route path='/cadastrar' component={SignUp} exact />
       <Route path='/login' component={SignIn} exact />
       <ProtectedRoute path='/agendar' component={Scheduling} exact />
+      <ProtectedRoute
+        path='/cancelar-agendamento'
+        component={CancelScheduling}
+        exact
+      />
       <Route path='*' component={PageNotFound} />
     </Switch>
   );
