1eebb64054c5124ae365e4871adf6577cbb7f83a
Matheus Carvalho <matheus10277@gmail.com>
2021-09-13 11:42:54 -0300

    Implementando lógica para mostrar avatar do usuário

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -1,9 +1,17 @@
 import * as S from "./style";
 
-import avatar from '../../assets/avatars/avatar6.svg';
+import avatar1 from "../../assets/avatars/avatar1.svg";
+import avatar2 from "../../assets/avatars/avatar2.svg";
+import avatar3 from "../../assets/avatars/avatar3.svg";
+import avatar4 from "../../assets/avatars/avatar4.svg";
+import avatar5 from "../../assets/avatars/avatar5.svg";
+import avatar6 from "../../assets/avatars/avatar6.svg";
+import avatar7 from "../../assets/avatars/avatar7.svg";
+import avatar8 from "../../assets/avatars/avatar8.svg";
+import avatar9 from "../../assets/avatars/avatar9.svg";
 
 type UserInfo = {
-  avatar: string;
+  avatar: number;
   nome: string;
   cargo: string;
 };
@@ -14,10 +22,11 @@ type ProfileCardProps = {
 };
 
 export default function ProfileCard({ userInfo, avatarClick }: ProfileCardProps) {  
+  const avatars = [avatar1, avatar2, avatar3, avatar4, avatar5, avatar6, avatar7, avatar8, avatar9];
 
   return (
     <S.ProfileWrapper>
-      {userInfo.avatar && <img src={avatar} className="profile-avatar" onClick={avatarClick} alt="" />}
+      {userInfo.avatar && <img src={avatars[userInfo.avatar - 1]} className="profile-avatar" onClick={avatarClick} alt="" />}
 
       <S.ProfileTag className="blue"> #ProudToBeOrange</S.ProfileTag>
       <S.ProfileTag className="orange">{userInfo.nome}</S.ProfileTag>
